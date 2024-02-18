92d9989b19ba7f19ed3db5626a75385f4f0e6d39
Hououin Kyouma <inwazzin@gmail.com>
2024-02-18 18:08:15 +0100

    reinit

---
+++ packages/client/vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import path from 'path';
+import million from 'million/compiler';
+
+export default defineConfig({
+  base: './',
+  plugins: [
+    million.vite({}),
+    react(),
+    svgr(),
+  ],
+  server: {
+    port: 8080,
+    open: true,
+  },
+  build: {
+    outDir: '../build',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@layout': path.resolve(__dirname, './src/layout'),
+      '@typings': path.resolve(__dirname, './src/types'),
+      '@context': path.resolve(__dirname, './src/context'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+    },
+  },
+});
