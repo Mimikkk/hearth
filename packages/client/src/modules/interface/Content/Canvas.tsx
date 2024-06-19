ca4ddf578fb1e039a413aadaf8184a216482d29e
Daniel Zdancewicz <inwazzin@gmail.com>
2024-06-19 18:39:09 +0200

    add unavailable info

--- packages/client/src/modules/interface/Content/Canvas.tsx
+++ packages/client/src/modules/interface/Content/Canvas.tsx
@@ -12,17 +12,40 @@ const Backdrop = () => (
   </div>
 );
 
+const Unavailable = () => (
+  <div class="w-full h-full center flex flex-col">
+    <span class="text-primary-8 flex gap-1 center">
+      <Icon name="BiSolidCog" size="lg" class="h-6 w-6" />
+      <span class="text-primary-5">WebGPU</span> is unavailable under this browser
+    </span>
+    <span class="text-primary-4">
+      Availability at:{' '}
+      <a
+        class="active:text-primary-5 hover:text-primary-4 text-primary-5 visited:text-accent-5 transition-all"
+        href="https://caniuse.com/webgpu"
+      >
+        https://caniuse.com/webgpu
+      </a>
+    </span>
+    <Icon name="RiUserFacesEmotionSadLine" size="3xl" class="mt-4 !h-36 !w-36" />
+  </div>
+);
+
 export const Canvas = () => {
   const { selectedExample } = useContent();
 
+  const isWebGpuAvailable = 'gpu' in navigator;
+
   return (
     <div class="w-full h-full rounded-sm border border-primary-3">
-      <Show when={selectedExample()} fallback={<Backdrop />}>
-        <object
-          class="w-full h-full rounded-sm"
-          type="text/html"
-          data={`src/modules/renderer/examples/${selectedExample()}.html`}
-        />
+      <Show when={isWebGpuAvailable} fallback={<Unavailable />}>
+        <Show when={selectedExample()} fallback={<Backdrop />}>
+          <object
+            class="w-full h-full rounded-sm"
+            type="text/html"
+            data={`src/modules/renderer/examples/${selectedExample()}.html`}
+          />
+        </Show>
       </Show>
     </div>
   );
