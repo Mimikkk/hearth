b94988642d07357979f5fffe9bd5cfa4490c57fa
Daniel Zdancewicz <inwazzin@gmail.com>
2024-07-01 19:18:35 +0200

    code view

--- packages/client/src/modules/interface/Content/CodeView.tsx
+++ packages/client/src/modules/interface/Content/CodeView.tsx
@@ -2,26 +2,33 @@ import { createEffect } from 'solid-js';
 import { editor } from 'monaco-editor';
 import '../../renderer/examples/utilities/monaco-vite.js';
 
+const readCodeFile = async (src: string) => {
+  const result = await fetch(src);
+
+  let code = await result.text();
+  const [, string] = code.match(/export default "(.*)";/) || [];
+  if (string) code = string;
+
+  code = code.replace(/\\r?\\n/g, '\n');
+  code = code.replace(/\/\/#.*$/, '');
+
+  return code;
+};
+
 export const CodeView = (props: { src: string }) => {
   let ref!: HTMLDivElement;
   const element = <div ref={ref} class="w-full h-full rounded-sm overflow-hidden" />;
 
-  createEffect(async () => {
-    const res = await fetch(props.src);
-    const code = await res.text();
-
-    monitor.setValue(code);
-  });
+  createEffect(async () => monitor.setValue(await readCodeFile(`${props.src}?src`)));
 
   const monitor = editor.create(ref, {
-    value: 'loading...',
     language: 'typescript',
     theme: 'vs-dark',
     automaticLayout: true,
     minimap: { enabled: false },
-    'semanticHighlighting.enabled': true,
+    domReadOnly: true,
     readOnly: true,
-    readOnlyMessage: { value: 'Preview only' },
+    'semanticHighlighting.enabled': true,
   });
 
   return element;
