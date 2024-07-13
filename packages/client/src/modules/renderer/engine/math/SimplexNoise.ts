a395269f369e122915af8d37a283f1f047df838e
Daniel Zdancewicz <inwazzin@gmail.com>
2024-07-13 23:25:03 +0200

    simplex

--- packages/client/src/modules/renderer/engine/math/SimplexNoise.ts
+++ packages/client/src/modules/renderer/engine/math/SimplexNoise.ts
@@ -1,220 +1,224 @@
+const grad3 = [
+  [1, 1, 0],
+  [-1, 1, 0],
+  [1, -1, 0],
+  [-1, -1, 0],
+  [1, 0, 1],
+  [-1, 0, 1],
+  [1, 0, -1],
+  [-1, 0, -1],
+  [0, 1, 1],
+  [0, -1, 1],
+  [0, 1, -1],
+  [0, -1, -1],
+];
+const grad4 = [
+  [0, 1, 1, 1],
+  [0, 1, 1, -1],
+  [0, 1, -1, 1],
+  [0, 1, -1, -1],
+  [0, -1, 1, 1],
+  [0, -1, 1, -1],
+  [0, -1, -1, 1],
+  [0, -1, -1, -1],
+  [1, 0, 1, 1],
+  [1, 0, 1, -1],
+  [1, 0, -1, 1],
+  [1, 0, -1, -1],
+  [-1, 0, 1, 1],
+  [-1, 0, 1, -1],
+  [-1, 0, -1, 1],
+  [-1, 0, -1, -1],
+  [1, 1, 0, 1],
+  [1, 1, 0, -1],
+  [1, -1, 0, 1],
+  [1, -1, 0, -1],
+  [-1, 1, 0, 1],
+  [-1, 1, 0, -1],
+  [-1, -1, 0, 1],
+  [-1, -1, 0, -1],
+  [1, 1, 1, 0],
+  [1, 1, -1, 0],
+  [1, -1, 1, 0],
+  [1, -1, -1, 0],
+  [-1, 1, 1, 0],
+  [-1, 1, -1, 0],
+  [-1, -1, 1, 0],
+  [-1, -1, -1, 0],
+];
+const simplex = [
+  [0, 1, 2, 3],
+  [0, 1, 3, 2],
+  [0, 0, 0, 0],
+  [0, 2, 3, 1],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [1, 2, 3, 0],
+  [0, 2, 1, 3],
+  [0, 0, 0, 0],
+  [0, 3, 1, 2],
+  [0, 3, 2, 1],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [1, 3, 2, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [1, 2, 0, 3],
+  [0, 0, 0, 0],
+  [1, 3, 0, 2],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [2, 3, 0, 1],
+  [2, 3, 1, 0],
+  [1, 0, 2, 3],
+  [1, 0, 3, 2],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [2, 0, 3, 1],
+  [0, 0, 0, 0],
+  [2, 1, 3, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [2, 0, 1, 3],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [3, 0, 1, 2],
+  [3, 0, 2, 1],
+  [0, 0, 0, 0],
+  [3, 1, 2, 0],
+  [2, 1, 0, 3],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [3, 1, 0, 2],
+  [0, 0, 0, 0],
+  [3, 2, 0, 1],
+  [3, 2, 1, 0],
+];
+
+type RandomFn = () => number;
+
 export class SimplexNoise {
-  grad3: number[][];
-  grad4: number[][];
-  p: number[];
-  perm: number[];
-  simplex: number[][];
+  noiseTable: number[];
+  permutationTable: number[];
 
-  constructor(r = Math) {
-    this.grad3 = [
-      [1, 1, 0],
-      [-1, 1, 0],
-      [1, -1, 0],
-      [-1, -1, 0],
-      [1, 0, 1],
-      [-1, 0, 1],
-      [1, 0, -1],
-      [-1, 0, -1],
-      [0, 1, 1],
-      [0, -1, 1],
-      [0, 1, -1],
-      [0, -1, -1],
-    ];
+  constructor(random: RandomFn = Math.random) {
+    this.noiseTable = [];
 
-    this.grad4 = [
-      [0, 1, 1, 1],
-      [0, 1, 1, -1],
-      [0, 1, -1, 1],
-      [0, 1, -1, -1],
-      [0, -1, 1, 1],
-      [0, -1, 1, -1],
-      [0, -1, -1, 1],
-      [0, -1, -1, -1],
-      [1, 0, 1, 1],
-      [1, 0, 1, -1],
-      [1, 0, -1, 1],
-      [1, 0, -1, -1],
-      [-1, 0, 1, 1],
-      [-1, 0, 1, -1],
-      [-1, 0, -1, 1],
-      [-1, 0, -1, -1],
-      [1, 1, 0, 1],
-      [1, 1, 0, -1],
-      [1, -1, 0, 1],
-      [1, -1, 0, -1],
-      [-1, 1, 0, 1],
-      [-1, 1, 0, -1],
-      [-1, -1, 0, 1],
-      [-1, -1, 0, -1],
-      [1, 1, 1, 0],
-      [1, 1, -1, 0],
-      [1, -1, 1, 0],
-      [1, -1, -1, 0],
-      [-1, 1, 1, 0],
-      [-1, 1, -1, 0],
-      [-1, -1, 1, 0],
-      [-1, -1, -1, 0],
-    ];
-
-    this.p = [];
-
-    for (let i = 0; i < 256; i++) {
-      this.p[i] = Math.floor(r.random() * 256);
-    }
+    for (let i = 0; i < 256; i++) this.noiseTable[i] = Math.floor(random() * 256);
 
     // To remove the need for index wrapping, double the permutation table length
-    this.perm = [];
-
-    for (let i = 0; i < 512; i++) {
-      this.perm[i] = this.p[i & 255];
-    }
-
-    // A lookup table to traverse the simplex around a given point in 4D.
-    // Details can be found where this table is used, in the 4D noise method.
-    this.simplex = [
-      [0, 1, 2, 3],
-      [0, 1, 3, 2],
-      [0, 0, 0, 0],
-      [0, 2, 3, 1],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [1, 2, 3, 0],
-      [0, 2, 1, 3],
-      [0, 0, 0, 0],
-      [0, 3, 1, 2],
-      [0, 3, 2, 1],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [1, 3, 2, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [1, 2, 0, 3],
-      [0, 0, 0, 0],
-      [1, 3, 0, 2],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [2, 3, 0, 1],
-      [2, 3, 1, 0],
-      [1, 0, 2, 3],
-      [1, 0, 3, 2],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [2, 0, 3, 1],
-      [0, 0, 0, 0],
-      [2, 1, 3, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [2, 0, 1, 3],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [3, 0, 1, 2],
-      [3, 0, 2, 1],
-      [0, 0, 0, 0],
-      [3, 1, 2, 0],
-      [2, 1, 0, 3],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [3, 1, 0, 2],
-      [0, 0, 0, 0],
-      [3, 2, 0, 1],
-      [3, 2, 1, 0],
-    ];
+    this.permutationTable = [];
+    for (let i = 0; i < 512; i++) this.permutationTable[i] = this.noiseTable[i & 255];
   }
 
-  dot(g: number[], x: number, y: number): number {
-    return g[0] * x + g[1] * y;
+  static create(random: RandomFn): SimplexNoise {
+    return new SimplexNoise(random);
   }
 
-  dot3(g: number[], x: number, y: number, z: number): number {
-    return g[0] * x + g[1] * y + g[2] * z;
+  dot2([a, b]: number[] | [number, number], x: number, y: number): number {
+    return a * x + b * y;
   }
 
-  dot4(g: number[], x: number, y: number, z: number, w: number): number {
-    return g[0] * x + g[1] * y + g[2] * z + g[3] * w;
+  dot3([a, b, c]: number[] | [number, number, number], x: number, y: number, z: number): number {
+    return a * x + b * y + c * z;
   }
 
-  noise(xin: number, yin: number): number {
-    let n0; // Noise contributions from the three corners
+  dot4([a, b, c, d]: number[] | [number, number, number, number], x: number, y: number, z: number, w: number): number {
+    return a * x + b * y + c * z + d * w;
+  }
+
+  noise2d(x: number, y: number): number {
+    // Noise contributions from the three corners
+    let n0;
     let n1;
     let n2;
+
     // Skew the input space to determine which simplex cell we're in
     const F2 = 0.5 * (Math.sqrt(3.0) - 1.0);
-    const s = (xin + yin) * F2; // Hairy factor for 2D
-    const i = Math.floor(xin + s);
-    const j = Math.floor(yin + s);
+    const s = (x + y) * F2; // Hairy factor for 2D
+    const i = Math.floor(x + s);
+    const j = Math.floor(y + s);
     const G2 = (3.0 - Math.sqrt(3.0)) / 6.0;
     const t = (i + j) * G2;
-    const X0 = i - t; // Unskew the cell origin back to (x,y) space
+
+    // Unskew the cell origin back to (x,y) space
+    const X0 = i - t;
     const Y0 = j - t;
-    const x0 = xin - X0; // The x,y distances from the cell origin
-    const y0 = yin - Y0;
+
+    // The x,y distances from the cell origin
+    const x0 = x - X0;
+    const y0 = y - Y0;
 
     // For the 2D case, the simplex shape is an equilateral triangle.
     // Determine which simplex we are in.
-    let i1; // Offsets for second (middle) corner of simplex in (i,j) coords
-
-    let j1;
+    // Offsets for second (middle) corner of simplex in (i,j) coords
+    let i1: number;
+    let j1: number;
+    // upper triangle, YX order: (0,0)->(0,1)->(1,1)
     if (x0 > y0) {
       i1 = 1;
       j1 = 0;
-
-      // lower triangle, XY order: (0,0)->(1,0)->(1,1)
-    } else {
+    }
+    // lower triangle, XY order: (0,0)->(1,0)->(1,1)
+    else {
       i1 = 0;
       j1 = 1;
-    } // upper triangle, YX order: (0,0)->(0,1)->(1,1)
+    }
 
     // A step of (1,0) in (i,j) means a step of (1-c,-c) in (x,y), and
     // a step of (0,1) in (i,j) means a step of (-c,1-c) in (x,y), where
     // c = (3-sqrt(3))/6
-    const x1 = x0 - i1 + G2; // Offsets for middle corner in (x,y) unskewed coords
+    // Offsets for middle corner in (x,y) unskewed coords
+    const x1 = x0 - i1 + G2;
     const y1 = y0 - j1 + G2;
-    const x2 = x0 - 1.0 + 2.0 * G2; // Offsets for last corner in (x,y) unskewed coords
+    // Offsets for last corner in (x,y) unskewed coords
+    const x2 = x0 - 1.0 + 2.0 * G2;
     const y2 = y0 - 1.0 + 2.0 * G2;
     // Work out the hashed gradient indices of the three simplex corners
     const ii = i & 255;
     const jj = j & 255;
-    const gi0 = this.perm[ii + this.perm[jj]] % 12;
-    const gi1 = this.perm[ii + i1 + this.perm[jj + j1]] % 12;
-    const gi2 = this.perm[ii + 1 + this.perm[jj + 1]] % 12;
+    const gi0 = this.permutationTable[ii + this.permutationTable[jj]] % 12;
+    const gi1 = this.permutationTable[ii + i1 + this.permutationTable[jj + j1]] % 12;
+    const gi2 = this.permutationTable[ii + 1 + this.permutationTable[jj + 1]] % 12;
     // Calculate the contribution from the three corners
     let t0 = 0.5 - x0 * x0 - y0 * y0;
     if (t0 < 0) n0 = 0.0;
     else {
       t0 *= t0;
-      n0 = t0 * t0 * this.dot(this.grad3[gi0], x0, y0); // (x,y) of grad3 used for 2D gradient
+      // (x,y) of grad3 used for 2D gradient
+      n0 = t0 * t0 * this.dot2(grad3[gi0], x0, y0);
     }
 
     let t1 = 0.5 - x1 * x1 - y1 * y1;
     if (t1 < 0) n1 = 0.0;
     else {
       t1 *= t1;
-      n1 = t1 * t1 * this.dot(this.grad3[gi1], x1, y1);
+      n1 = t1 * t1 * this.dot2(grad3[gi1], x1, y1);
     }
 
     let t2 = 0.5 - x2 * x2 - y2 * y2;
     if (t2 < 0) n2 = 0.0;
     else {
       t2 *= t2;
-      n2 = t2 * t2 * this.dot(this.grad3[gi2], x2, y2);
+      n2 = t2 * t2 * this.dot2(grad3[gi2], x2, y2);
     }
 
     // Add contributions from each corner to get the final noise value.
@@ -222,33 +226,40 @@ export class SimplexNoise {
     return 70.0 * (n0 + n1 + n2);
   }
 
-  noise3d(xin: number, yin: number, zin: number): number {
-    let n0; // Noise contributions from the four corners
+  noise3d(x: number, y: number, z: number): number {
+    // Noise contributions from the four corners
+    let n0;
     let n1;
     let n2;
     let n3;
     // Skew the input space to determine which simplex cell we're in
     const F3 = 1.0 / 3.0;
-    const s = (xin + yin + zin) * F3; // Very nice and simple skew factor for 3D
-    const i = Math.floor(xin + s);
-    const j = Math.floor(yin + s);
-    const k = Math.floor(zin + s);
-    const G3 = 1.0 / 6.0; // Very nice and simple unskew factor, too
+    // Very nice and simple skew factor for 3D
+    const s = (x + y + z) * F3;
+    const i = Math.floor(x + s);
+    const j = Math.floor(y + s);
+    const k = Math.floor(z + s);
+    // Very nice and simple unskew factor, too
+    const G3 = 1.0 / 6.0;
     const t = (i + j + k) * G3;
-    const X0 = i - t; // Unskew the cell origin back to (x,y,z) space
+    // Unskew the cell origin back to (x,y,z) space
+    const X0 = i - t;
     const Y0 = j - t;
     const Z0 = k - t;
-    const x0 = xin - X0; // The x,y,z distances from the cell origin
-    const y0 = yin - Y0;
-    const z0 = zin - Z0;
+    // The x,y,z distances from the cell origin
+    const x0 = x - X0;
+    const y0 = y - Y0;
+    const z0 = z - Z0;
 
     // For the 3D case, the simplex shape is a slightly irregular tetrahedron.
     // Determine which simplex we are in.
-    let i1; // Offsets for second corner of simplex in (i,j,k) coords
+    // Offsets for second corner of simplex in (i,j,k) coords
+    let i1;
 
     let j1;
     let k1;
-    let i2; // Offsets for third corner of simplex in (i,j,k) coords
+    // Offsets for third corner of simplex in (i,j,k) coords
+    let i2;
     let j2;
     let k2;
     if (x0 >= y0) {
@@ -313,50 +324,53 @@ export class SimplexNoise {
     // a step of (0,1,0) in (i,j,k) means a step of (-c,1-c,-c) in (x,y,z), and
     // a step of (0,0,1) in (i,j,k) means a step of (-c,-c,1-c) in (x,y,z), where
     // c = 1/6.
-    const x1 = x0 - i1 + G3; // Offsets for second corner in (x,y,z) coords
+    // Offsets for second corner in (x,y,z) coords
+    const x1 = x0 - i1 + G3;
     const y1 = y0 - j1 + G3;
     const z1 = z0 - k1 + G3;
-    const x2 = x0 - i2 + 2.0 * G3; // Offsets for third corner in (x,y,z) coords
+    // Offsets for third corner in (x,y,z) coords
+    const x2 = x0 - i2 + 2.0 * G3;
     const y2 = y0 - j2 + 2.0 * G3;
     const z2 = z0 - k2 + 2.0 * G3;
-    const x3 = x0 - 1.0 + 3.0 * G3; // Offsets for last corner in (x,y,z) coords
+    // Offsets for last corner in (x,y,z) coords
+    const x3 = x0 - 1.0 + 3.0 * G3;
     const y3 = y0 - 1.0 + 3.0 * G3;
     const z3 = z0 - 1.0 + 3.0 * G3;
     // Work out the hashed gradient indices of the four simplex corners
     const ii = i & 255;
     const jj = j & 255;
     const kk = k & 255;
-    const gi0 = this.perm[ii + this.perm[jj + this.perm[kk]]] % 12;
-    const gi1 = this.perm[ii + i1 + this.perm[jj + j1 + this.perm[kk + k1]]] % 12;
-    const gi2 = this.perm[ii + i2 + this.perm[jj + j2 + this.perm[kk + k2]]] % 12;
-    const gi3 = this.perm[ii + 1 + this.perm[jj + 1 + this.perm[kk + 1]]] % 12;
+    const gi0 = this.permutationTable[ii + this.permutationTable[jj + this.permutationTable[kk]]] % 12;
+    const gi1 = this.permutationTable[ii + i1 + this.permutationTable[jj + j1 + this.permutationTable[kk + k1]]] % 12;
+    const gi2 = this.permutationTable[ii + i2 + this.permutationTable[jj + j2 + this.permutationTable[kk + k2]]] % 12;
+    const gi3 = this.permutationTable[ii + 1 + this.permutationTable[jj + 1 + this.permutationTable[kk + 1]]] % 12;
     // Calculate the contribution from the four corners
     let t0 = 0.6 - x0 * x0 - y0 * y0 - z0 * z0;
     if (t0 < 0) n0 = 0.0;
     else {
       t0 *= t0;
-      n0 = t0 * t0 * this.dot3(this.grad3[gi0], x0, y0, z0);
+      n0 = t0 * t0 * this.dot3(grad3[gi0], x0, y0, z0);
     }
 
     let t1 = 0.6 - x1 * x1 - y1 * y1 - z1 * z1;
     if (t1 < 0) n1 = 0.0;
     else {
       t1 *= t1;
-      n1 = t1 * t1 * this.dot3(this.grad3[gi1], x1, y1, z1);
+      n1 = t1 * t1 * this.dot3(grad3[gi1], x1, y1, z1);
     }
 
     let t2 = 0.6 - x2 * x2 - y2 * y2 - z2 * z2;
     if (t2 < 0) n2 = 0.0;
     else {
       t2 *= t2;
-      n2 = t2 * t2 * this.dot3(this.grad3[gi2], x2, y2, z2);
+      n2 = t2 * t2 * this.dot3(grad3[gi2], x2, y2, z2);
     }
 
     let t3 = 0.6 - x3 * x3 - y3 * y3 - z3 * z3;
     if (t3 < 0) n3 = 0.0;
     else {
       t3 *= t3;
-      n3 = t3 * t3 * this.dot3(this.grad3[gi3], x3, y3, z3);
+      n3 = t3 * t3 * this.dot3(grad3[gi3], x3, y3, z3);
     }
 
     // Add contributions from each corner to get the final noise value.
@@ -365,31 +379,33 @@ export class SimplexNoise {
   }
 
   noise4d(x: number, y: number, z: number, w: number): number {
-    // For faster and easier lookups
-    const grad4 = this.grad4;
-    const simplex = this.simplex;
-    const perm = this.perm;
+    const perm = this.permutationTable;
 
     // The skewing and unskewing factors are hairy again for the 4D case
     const F4 = (Math.sqrt(5.0) - 1.0) / 4.0;
     const G4 = (5.0 - Math.sqrt(5.0)) / 20.0;
-    let n0; // Noise contributions from the five corners
+    // Noise contributions from the five corners
+    let n0;
     let n1;
     let n2;
     let n3;
     let n4;
     // Skew the (x,y,z,w) space to determine which cell of 24 simplices we're in
-    const s = (x + y + z + w) * F4; // Factor for 4D skewing
+    // Factor for 4D skewing
+    const s = (x + y + z + w) * F4;
     const i = Math.floor(x + s);
     const j = Math.floor(y + s);
     const k = Math.floor(z + s);
     const l = Math.floor(w + s);
-    const t = (i + j + k + l) * G4; // Factor for 4D unskewing
-    const X0 = i - t; // Unskew the cell origin back to (x,y,z,w) space
+    // Factor for 4D unskewing
+    const t = (i + j + k + l) * G4;
+    // Unskew the cell origin back to (x,y,z,w) space
+    const X0 = i - t;
     const Y0 = j - t;
     const Z0 = k - t;
     const W0 = l - t;
-    const x0 = x - X0; // The x,y,z,w distances from the cell origin
+    // The x,y,z,w distances from the cell origin
+    const x0 = x - X0;
     const y0 = y - Y0;
     const z0 = z - Z0;
     const w0 = w - W0;
@@ -430,19 +446,23 @@ export class SimplexNoise {
     const k3 = simplex[c][2] >= 1 ? 1 : 0;
     const l3 = simplex[c][3] >= 1 ? 1 : 0;
     // The fifth corner has all coordinate offsets = 1, so no need to look that up.
-    const x1 = x0 - i1 + G4; // Offsets for second corner in (x,y,z,w) coords
+    // Offsets for second corner in (x,y,z,w) coords
+    const x1 = x0 - i1 + G4;
     const y1 = y0 - j1 + G4;
     const z1 = z0 - k1 + G4;
     const w1 = w0 - l1 + G4;
-    const x2 = x0 - i2 + 2.0 * G4; // Offsets for third corner in (x,y,z,w) coords
+    // Offsets for third corner in (x,y,z,w) coords
+    const x2 = x0 - i2 + 2.0 * G4;
     const y2 = y0 - j2 + 2.0 * G4;
     const z2 = z0 - k2 + 2.0 * G4;
     const w2 = w0 - l2 + 2.0 * G4;
-    const x3 = x0 - i3 + 3.0 * G4; // Offsets for fourth corner in (x,y,z,w) coords
+    // Offsets for fourth corner in (x,y,z,w) coords
+    const x3 = x0 - i3 + 3.0 * G4;
     const y3 = y0 - j3 + 3.0 * G4;
     const z3 = z0 - k3 + 3.0 * G4;
     const w3 = w0 - l3 + 3.0 * G4;
-    const x4 = x0 - 1.0 + 4.0 * G4; // Offsets for last corner in (x,y,z,w) coords
+    // Offsets for last corner in (x,y,z,w) coords
+    const x4 = x0 - 1.0 + 4.0 * G4;
     const y4 = y0 - 1.0 + 4.0 * G4;
     const z4 = z0 - 1.0 + 4.0 * G4;
     const w4 = w0 - 1.0 + 4.0 * G4;
