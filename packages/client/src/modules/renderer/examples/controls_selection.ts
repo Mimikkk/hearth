ba1cbc1220cd65f0ba20a9d57d211da3f450a897
Daniel Zdancewicz <inwazzin@gmail.com>
2024-07-15 14:01:45 +0200

    selection example

--- packages/client/src/modules/renderer/examples/controls_selection.ts
+++ packages/client/src/modules/renderer/examples/controls_selection.ts
@@ -116,7 +116,13 @@ renderer.parameters.canvas.addEventListener('pointerup', event => {
 });
 
 const ui = UI.create('Selection')
+  .text('Selected count:', () => selection.collection.length)
   .text('Mouse start (x, y):', () => selection.start.x.toFixed(2) + ', ' + selection.start.y.toFixed(2))
   .text('Mouse end (x, y):', () => selection.end.x.toFixed(2) + ', ' + selection.end.y.toFixed(2))
-  .text('Selected count:', () => selection.collection.length)
-  .action('Log selected', () => console.log(selection.collection));
+  .action('Log selection', () => console.log(selection.collection))
+  .action('Clear selection', () => {
+    for (const object of selection.collection) {
+      (object.material as MeshLambertMaterial).emissive.set(0x000000);
+    }
+    selection.collection.length = 0;
+  });
