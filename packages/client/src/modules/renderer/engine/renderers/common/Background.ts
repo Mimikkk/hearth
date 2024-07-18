b45c9132ba2cbbf7e2699b53c57c0300a4204a5a
Daniel Zdancewicz <inwazzin@gmail.com>
2024-07-18 22:15:51 +0200

    bg

--- packages/client/src/modules/renderer/engine/renderers/common/Background.ts
+++ packages/client/src/modules/renderer/engine/renderers/common/Background.ts
@@ -72,7 +72,7 @@ class Background extends DataMap<any, any> {
         backgroundMesh.frustumCulled = false;
 
         backgroundMesh.onBeforeRender = function (renderer: Renderer, scene: Scene, camera: Camera) {
-          this.matrixWorld.copyPosition(camera.matrixWorld);
+          this.matrixWorld.setPosition(camera.matrixWorld);
         };
       }
 
