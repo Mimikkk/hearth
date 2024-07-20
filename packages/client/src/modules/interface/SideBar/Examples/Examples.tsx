ae9afa2b00e70ba9c69687429ccc2169b623ace0
Daniel Zdancewicz <inwazzin@gmail.com>
2024-07-20 03:04:54 +0200

    Revert "autfill clear"
    This reverts commit 0ffb9fbed5ebceec9efe7d32f21f3a127064280e.

--- packages/client/src/modules/interface/SideBar/Examples/Examples.tsx
+++ packages/client/src/modules/interface/SideBar/Examples/Examples.tsx
@@ -11,7 +11,6 @@ import { useContent } from '@modules/managment/useContent.js';
 import { createEffectListener } from '@logic/createListener.js';
 import { ExampleName } from '@modules/renderer/examples/examples.js';
 import { Shortcut } from '@components/forms/Shortcut/Shortcut.js';
-import { ButtonIcon } from '@components/buttons/ButtonIcon/ButtonIcon.js';
 
 const FocusShortcut = () => (
   <Shortcut class="absolute right-0 pr-1 opacity-50">
@@ -125,14 +124,7 @@ export const Examples = () => {
           onChange={setQuery}
           onFocusChange={setIsFocused}
           after={
-            <Show
-              when={!query().length && !isFocused()}
-              fallback={
-                <Show when={query().length}>
-                  <ButtonIcon onClick={() => setQuery('')} icon="ImCross" size="xs" variant="text" />
-                </Show>
-              }
-            >
+            <Show when={!isFocused()}>
               <FocusShortcut />
             </Show>
           }
