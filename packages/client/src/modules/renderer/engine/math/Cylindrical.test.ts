161f882e35cdfa285be58b319c1cb5e8659a8c03
Daniel Zdancewicz <inwazzin@gmail.com>
2024-07-20 03:05:42 +0200

    Revert "Revert "cylindrical""
    This reverts commit 12ccbbec645800e1a77beb1e0965880c253cd06d.

--- packages/client/src/modules/renderer/engine/math/Cylindrical.test.ts
+++ packages/client/src/modules/renderer/engine/math/Cylindrical.test.ts
@@ -1,35 +1,35 @@
 import { describe, expect, it } from 'vitest';
 import { IVec3 } from './Vector3.js';
 import { Cylindrical } from './Cylindrical.js';
+import vec3 = IVec3.vec3;
 
 describe('Math - Cylindrical', () => {
   it('Instancing', () => {
     const cylindrical = Cylindrical.empty();
-    expect(cylindrical).toEqual({ radius: 0, theta: 0, y: 0 });
+    expect(cylindrical).toEqual({ radius: 0, theta: 0, height: 0 });
 
     const radius = 10.0;
     const theta = Math.PI;
-    const y = 5;
-    expect(Cylindrical.set(cylindrical, radius, theta, y)).toBe(cylindrical);
-    expect(cylindrical).toEqual({ radius, theta, y });
+    const height = 5;
 
-    const cloned = Cylindrical.clone(cylindrical);
+    expect(cylindrical.fill(radius, theta, height)).toBe(cylindrical);
+    expect(cylindrical).toEqual(Cylindrical.new(radius, theta, height));
+
+    const cloned = cylindrical.clone();
     expect(cloned).not.toBe(cylindrical);
     expect(cloned).toEqual(cylindrical);
   });
 
   it('fromCartesian', () => {
-    const a = Cylindrical.create(1, 1, 1);
-    const b = IVec3.create(0, 0, 0);
-    const c = IVec3.create(3, -1, -3);
-    const expected = Cylindrical.create(Math.sqrt(9 + 9), Math.atan2(3, -3), -1);
+    const cylindrical = Cylindrical.new(1, 1, 1);
+    const expected = Cylindrical.new(Math.sqrt(9 + 9), Math.atan2(3, -3), -1);
 
-    expect(Cylindrical.fillCartesian(a, b)).toBe(a);
-    expect(a).toEqual({ radius: 0, theta: 0, y: 0 });
+    expect(cylindrical.fillCartesian(vec3(0, 0, 0))).toBe(cylindrical);
+    expect(cylindrical).toEqual(Cylindrical.new(0, 0, 0));
 
-    expect(Cylindrical.fillCartesian(a, c)).toBe(a);
-    expect(a.radius).closeTo(expected.radius, Number.EPSILON);
-    expect(a.theta).closeTo(expected.theta, Number.EPSILON);
-    expect(a.y).closeTo(expected.y, Number.EPSILON);
+    expect(cylindrical.fillCartesian(vec3(3, -1, -3))).toBe(cylindrical);
+    expect(cylindrical.radius).closeTo(expected.radius, Number.EPSILON);
+    expect(cylindrical.theta).closeTo(expected.theta, Number.EPSILON);
+    expect(cylindrical.height).closeTo(expected.height, Number.EPSILON);
   });
 });
