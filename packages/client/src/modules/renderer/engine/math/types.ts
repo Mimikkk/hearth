32ba455c33f091e8746e9d3c02cb95c0425c153a
Daniel Zdancewicz <inwazzin@gmail.com>
2024-07-20 03:06:35 +0200

    Revert "const"
    This reverts commit cc676ea21d0162608f34328b1a61fd7c2a68009e.

--- packages/client/src/modules/renderer/engine/math/types.ts
+++
@@ -1,7 +0,0 @@
-export type Const<T> = T extends (...args: never[]) => unknown
-  ? T
-  : T extends Map<infer K, infer V>
-    ? ReadonlyMap<Const<K>, Const<V>>
-    : T extends Set<infer V>
-      ? ReadonlySet<Const<V>>
-      : { readonly [P in keyof T]: Const<T[P]> };
