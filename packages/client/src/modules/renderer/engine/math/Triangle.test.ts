c6181659e3f1f63e108ba7b70b88951f0971e7ef
Daniel Zdancewicz <inwazzin@gmail.com>
2024-07-20 03:06:13 +0200

    Revert "triangle tests"
    This reverts commit fdfec677852e4b53c84134ef020e5bf6dfcd39e8.

--- packages/client/src/modules/renderer/engine/math/Triangle.test.ts
+++ packages/client/src/modules/renderer/engine/math/Triangle.test.ts
@@ -1,12 +1,12 @@
 import { describe, expect, it } from 'vitest';
-import { Triangle, Triangle_ } from './Triangle.js';
-import { Vec3, Vector3 } from './Vector3.ts';
+import { Triangle_ } from './Triangle.js';
+import { Vec3 } from './Vector3.ts';
 import { BufferAttribute } from '@modules/renderer/engine/core/BufferAttribute.js';
 import { Plane_ } from './Plane.ts';
 import { Box3_ } from '@modules/renderer/engine/math/Box3.js';
 
 describe('Maths', () => {
-  it('Instancing', () => {
+  it.only('Instancing', () => {
     const triangle = Triangle_.empty();
     expect(triangle).toEqual({
       a: { x: 0, y: 0, z: 0 },
@@ -36,7 +36,7 @@ describe('Maths', () => {
     expect(copy.c).toBe(triangle.c);
   });
 
-  it('fromPointsAndIndices', () => {
+  it.only('fromPointsAndIndices', () => {
     const points = [Vec3.negate(Vec3.create(1, 1, 1)), Vec3.create(1, 1, 1), Vec3.create(2, 2, 2)];
     const triangle = Triangle_.fromPointsAndIndices(points, 1, 0, 2);
 
@@ -50,7 +50,7 @@ describe('Maths', () => {
     expect(triangle.c).not.toBe(points[2]);
   });
 
-  it('fromAttributeAndIndices', () => {
+  it.only('fromAttributeAndIndices', () => {
     const attribute = new BufferAttribute(new Float32Array([1, 1, 1, -1, -1, -1, 2, 2, 2]), 3);
 
     const triangle = Triangle_.fromAttributeAndIndices(attribute, 1, 0, 2);
@@ -62,31 +62,9 @@ describe('Maths', () => {
     });
   });
 
-  it('interpolate', () => {
-    const from = Triangle_.create(Vec3.create(0, 0, 0), Vec3.create(1, 0, 0), Vec3.create(0, 1, 0));
-    const to = Triangle_.create(Vec3.create(2, 0, 0), Vec3.create(0, 0, 0), Vec3.create(0, 0, 2));
+  it('interpolate', () => {});
 
-    const result = Vec3.empty();
-    Triangle_.interpolate_(from, to, Vec3.create(0, 0, 0), result);
-    expect(result).toEqual(Vec3.create(2, 0, 0));
-
-    Triangle_.interpolate_(from, to, Vec3.create(1, 0, 0), result);
-    expect(result).toEqual(Vec3.create(0, 0, 0));
-
-    Triangle_.interpolate_(from, to, Vec3.create(0, 1, 0), result);
-    expect(result).toEqual(Vec3.create(0, 0, 2));
-
-    Triangle_.interpolate_(from, to, Vec3.create(0.5, 0.5, 0), result);
-    expect(result).toEqual(Vec3.create(0, 0, 1));
-
-    Triangle_.interpolate_(from, to, Vec3.create(1, 0, 0.5), result);
-    expect(result).toEqual(Vec3.create(0, 0, 0));
-
-    Triangle_.interpolate_(from, to, Vec3.create(0, 1, 0.5), result);
-    expect(result).toEqual(Vec3.create(0, 0, 2));
-  });
-
-  it('area', () => {
+  it.only('area', () => {
     let triangle = Triangle_.empty();
 
     expect(Triangle_.area(triangle)).toBe(0);
@@ -101,7 +79,7 @@ describe('Maths', () => {
     expect(Triangle_.area(triangle)).toBe(0);
   });
 
-  it('midpoint', () => {
+  it.only('midpoint', () => {
     let triangle = Triangle_.empty();
     const midpoint = Vec3.empty();
 
@@ -114,7 +92,7 @@ describe('Maths', () => {
     expect(Triangle_.midpoint_(triangle, midpoint)).toEqual(Vec3.create(2 / 3, 0, 2 / 3));
   });
 
-  it('normal', () => {
+  it.only('normal', () => {
     let triangle = Triangle_.empty();
     const normal = Vec3.empty();
 
@@ -127,7 +105,7 @@ describe('Maths', () => {
     expect(Triangle_.normal_(triangle, normal)).toEqual(Vec3.create(0, 1, 0));
   });
 
-  it('plane', () => {
+  it.only('plane', () => {
     let triangle = Triangle_.empty();
     const plane = Plane_.empty();
     const normal = Vec3.empty();
@@ -153,7 +131,7 @@ describe('Maths', () => {
     expect(Triangle_.normal_(triangle, normal)).toEqual(Vec3.create(0, 1, 0));
   });
 
-  it('barycoord', () => {
+  it.only('barycoord', () => {
     let a = Triangle_.empty();
 
     const barycoord = Vec3.empty();
@@ -188,7 +166,7 @@ describe('Maths', () => {
     expect(Vec3.distanceTo(barycoord, Vec3.create(1 / 3, 1 / 3, 1 / 3))).toBeCloseTo(0);
   });
 
-  it('containsVec', () => {
+  it.only('containsVec', () => {
     let a = Triangle_.empty();
     const midpoint = Vec3.empty();
 
@@ -213,7 +191,7 @@ describe('Maths', () => {
     expect(Triangle_.containsVec(a, Vec3.create(-1, -1, -1))).toBe(false);
   });
 
-  it('intersectsBox', () => {
+  it.only('intersectsBox', () => {
     const a = Box3_.create(1, 1, 1, 2, 2, 2);
     const b = Triangle_.create(Vec3.create(1.5, 1.5, 2.5), Vec3.create(2.5, 1.5, 1.5), Vec3.create(1.5, 2.5, 1.5));
     const c = Triangle_.create(Vec3.create(1.5, 1.5, 3.5), Vec3.create(3.5, 1.5, 1.5), Vec3.create(1.5, 1.5, 1.5));
@@ -229,42 +207,42 @@ describe('Maths', () => {
   });
 
   it('closestTo', () => {
-    const triangle = Triangle_.create(Vec3.create(-1, 0, 0), Vec3.create(1, 0, 0), Vec3.create(0, 1, 0));
-    const point = Vec3.empty();
+    const a = Triangle_.create(Vec3.create(-1, 0, 0), Vec3.create(1, 0, 0), Vec3.create(0, 1, 0));
+    const point = Vec3.create();
 
     // point lies inside the triangle
-    Triangle_.closestTo_(triangle, Vec3.create(0, 0.5, 0), point);
-    expect(point).toEqual(Vec3.create(0, 0.5, 0));
+    a.closestPointToPoint(Vec3.create(0, 0.5, 0), point);
+    expect(point.equals(Vec3.create(0, 0.5, 0))).toBe(true);
 
     // point lies on a vertex
-    Triangle_.closestTo_(triangle, triangle.a, point);
-    expect(point).toEqual(triangle.a);
+    a.closestPointToPoint(a.a, point);
+    expect(point.equals(a.a)).toBe(true);
 
-    Triangle_.closestTo_(triangle, triangle.b, point);
-    expect(point).toEqual(triangle.b);
+    a.closestPointToPoint(a.b, point);
+    expect(point.equals(a.b)).toBe(true);
 
-    Triangle_.closestTo_(triangle, triangle.c, point);
-    expect(point).toEqual(triangle.c);
+    a.closestPointToPoint(a.c, point);
+    expect(point.equals(a.c)).toBe(true);
 
     // point lies on an edge
-    Triangle_.closestTo_(triangle, Vec3.create(0, 0, 0), point);
-    expect(point).toEqual(Vec3.create(0, 0, 0));
+    a.closestPointToPoint(zero3.clone(), point);
+    expect(point.equals(zero3.clone())).toBe(true);
 
     // point lies outside the triangle
-    Triangle_.closestTo_(triangle, Vec3.create(-2, 0, 0), point);
-    expect(point).toEqual(Vec3.create(-1, 0, 0));
+    a.closestPointToPoint(Vec3.create(-2, 0, 0), point);
+    expect(point.equals(Vec3.create(-1, 0, 0))).toBe(true);
 
-    Triangle_.closestTo_(triangle, Vec3.create(2, 0, 0), point);
-    expect(point).toEqual(Vec3.create(1, 0, 0));
+    a.closestPointToPoint(Vec3.create(2, 0, 0), point);
+    expect(point.equals(Vec3.create(1, 0, 0))).toBe(true);
 
-    Triangle_.closestTo_(triangle, Vec3.create(0, 2, 0), point);
-    expect(point).toEqual(Vec3.create(0, 1, 0));
+    a.closestPointToPoint(Vec3.create(0, 2, 0), point);
+    expect(point.equals(Vec3.create(0, 1, 0))).toBe(true);
 
-    Triangle_.closestTo_(triangle, Vec3.create(0, -2, 0), point);
-    expect(point).toEqual(Vec3.create(0, 0, 0));
+    a.closestPointToPoint(Vec3.create(0, -2, 0), point);
+    expect(point.equals(Vec3.create(0, 0, 0))).toBe(true);
   });
 
-  it('isFrontFacing', () => {
+  it.only('isFrontFacing', () => {
     const triangle = Triangle_.empty();
     const direction = Vec3.empty();
     expect(Triangle_.isFrontFacing(triangle, direction)).toBe(false);
