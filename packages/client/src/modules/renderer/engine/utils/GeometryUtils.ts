592936a5db6e2b69b5cfa9f7fd9a5238228f6136
Daniel Zdancewicz <inwazzin@gmail.com>
2024-07-22 13:13:11 +0200

    hilbert

--- packages/client/src/modules/renderer/engine/utils/GeometryUtils.ts
+++ packages/client/src/modules/renderer/engine/utils/GeometryUtils.ts
@@ -12,6 +12,7 @@ export function hilbert3D(
   v5: number = 5,
   v6: number = 6,
   v7: number = 7,
+  into: Vec3[] = [],
 ): Vec3[] {
   const half = size / 2;
 
@@ -26,20 +27,18 @@ export function hilbert3D(
     new Vec3(center.x + half, center.y + half, center.z - half),
   ];
 
-  const vec = [vec_s[v0], vec_s[v1], vec_s[v2], vec_s[v3], vec_s[v4], vec_s[v5], vec_s[v6], vec_s[v7]];
-
   if (--iterations >= 0) {
-    return [
-      ...hilbert3D(vec[0], half, iterations, v0, v3, v4, v7, v6, v5, v2, v1),
-      ...hilbert3D(vec[1], half, iterations, v0, v7, v6, v1, v2, v5, v4, v3),
-      ...hilbert3D(vec[2], half, iterations, v0, v7, v6, v1, v2, v5, v4, v3),
-      ...hilbert3D(vec[3], half, iterations, v2, v3, v0, v1, v6, v7, v4, v5),
-      ...hilbert3D(vec[4], half, iterations, v2, v3, v0, v1, v6, v7, v4, v5),
-      ...hilbert3D(vec[5], half, iterations, v4, v3, v2, v5, v6, v1, v0, v7),
-      ...hilbert3D(vec[6], half, iterations, v4, v3, v2, v5, v6, v1, v0, v7),
-      ...hilbert3D(vec[7], half, iterations, v6, v5, v2, v1, v0, v3, v4, v7),
-    ];
+    hilbert3D(vec_s[0], half, iterations, v0, v3, v4, v7, v6, v5, v2, v1, into);
+    hilbert3D(vec_s[1], half, iterations, v0, v7, v6, v1, v2, v5, v4, v3, into);
+    hilbert3D(vec_s[2], half, iterations, v0, v7, v6, v1, v2, v5, v4, v3, into);
+    hilbert3D(vec_s[3], half, iterations, v2, v3, v0, v1, v6, v7, v4, v5, into);
+    hilbert3D(vec_s[4], half, iterations, v2, v3, v0, v1, v6, v7, v4, v5, into);
+    hilbert3D(vec_s[5], half, iterations, v4, v3, v2, v5, v6, v1, v0, v7, into);
+    hilbert3D(vec_s[6], half, iterations, v4, v3, v2, v5, v6, v1, v0, v7, into);
+    hilbert3D(vec_s[7], half, iterations, v6, v5, v2, v1, v0, v3, v4, v7, into);
+  } else {
+    into.push(vec_s[v0], vec_s[v1], vec_s[v2], vec_s[v3], vec_s[v4], vec_s[v5], vec_s[v6], vec_s[v7]);
   }
 
-  return vec;
+  return into;
 }
