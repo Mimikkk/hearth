b2c23bf489c85604c65c37a097a53a84aec2a73b
Daniel Zdancewicz <inwazzin@gmail.com>
2024-07-26 22:16:59 +0200

    color space

--- packages/client/src/modules/renderer/engine/nodes/shadernode/tslFn.ts
+++ packages/client/src/modules/renderer/engine/nodes/shadernode/tslFn.ts
@@ -1,6 +1,6 @@
 import { ShaderNode } from '@modules/renderer/engine/nodes/shadernode/ShaderNode.js';
 import { createShaderNodeObjects } from '@modules/renderer/engine/nodes/shadernode/CreateShaderNodeObject.js';
-import Node from '@modules/renderer/engine/nodes/core/Node.js';
+import { Node } from '@modules/renderer/engine/nodes/core/Node.js';
 import { TypeName } from '@modules/renderer/engine/nodes/builder/NodeBuilder.types.js';
 
 interface TslInput {
@@ -15,7 +15,7 @@ interface Layout {
 }
 
 interface TslFn {
-  (params: Node | Node[]): Node;
+  (params: Record<string, Node>): Node;
   shaderNode: ShaderNode;
   setLayout: (layout: Layout) => Function;
 }
@@ -24,20 +24,11 @@ export const tslFn = (jsFn: Function): TslFn => {
   const node = new ShaderNode(jsFn);
 
   const fn = (...params) => {
-    let inputs;
-
     createShaderNodeObjects(params);
-
-    if (Node.is(params[0])) {
-      inputs = [...params];
-    } else {
-      inputs = params[0];
-    }
-
-    return node.call(inputs);
+    return node.call(Node.is(params[0]) ? [...params] : params[0]);
   };
   fn.shaderNode = node;
-  fn.setLayout = layout => {
+  fn.setLayout = (layout: Layout) => {
     node.setLayout(layout);
     return fn;
   };
