bfd8b37927f778f5a7773b19b21d58b0f6e76452
Daniel Zdancewicz <inwazzin@gmail.com>
2024-07-27 18:41:05 +0200

    at

--- packages/client/src/modules/renderer/engine/animation/AnimationUtils.ts
+++ packages/client/src/modules/renderer/engine/animation/AnimationUtils.ts
@@ -1,11 +1,11 @@
 import type {
   ArrayMap,
   NumberArray,
+  NumberArrayConstructor,
   TypedArray,
-  TypedArrayConstructor,
 } from '@modules/renderer/engine/math/MathUtils.js';
 
-export const convertArray = <R extends TypedArrayConstructor>(array: NumberArray, type: R): ArrayMap<R> =>
+export const convertArray = <R extends NumberArrayConstructor>(array: NumberArray, type: R): ArrayMap<R> =>
   ('BYTES_PER_ELEMENT' in type ? new type(array) : Array.from(array)) as ArrayMap<R>;
 
 export const isTypedArray = (object: any): object is TypedArray =>
