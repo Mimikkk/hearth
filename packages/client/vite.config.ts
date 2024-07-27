150141423d1b6a0457c7b330682e1990d38004fe
Daniel Zdancewicz <inwazzin@gmail.com>
2024-07-27 23:07:33 +0200

    update

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -2,15 +2,16 @@ import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
 import svgr from 'vite-plugin-svgr';
 import path from 'path';
-import arraybuffer from 'vite-plugin-arraybuffer';
 
 const resolveAt = (relativePath: string) => path.resolve(__dirname, relativePath);
 const createAliases = (...aliases: [alias: string, path: string][]) =>
   Object.fromEntries(aliases.map(([alias, path]) => [alias, resolveAt(path)]));
 
+const prefix = `monaco-editor/esm/vs`;
+
 export default defineConfig({
   base: './',
-  plugins: [solid(), svgr(), arraybuffer()],
+  plugins: [solid(), svgr()],
   server: {
     port: 8080,
     open: false,
