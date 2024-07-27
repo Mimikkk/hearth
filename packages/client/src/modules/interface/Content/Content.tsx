6cc93741fd6f4499a0ff53c3923110ddc3b88340
Daniel Zdancewicz <inwazzin@gmail.com>
2024-07-27 19:02:54 +0200

    style info

--- packages/client/src/modules/interface/Content/Content.tsx
+++ packages/client/src/modules/interface/Content/Content.tsx
@@ -2,18 +2,25 @@ import { ButtonIcon } from '@components/buttons/ButtonIcon/ButtonIcon.js';
 import cx from 'clsx';
 import { SearchStorage } from '@logic/SearchStorage/SearchStorage.js';
 import { Canvas } from '@modules/interface/Content/Canvas.js';
+
 interface ContentProps {
   class?: string;
 }
 
-export const Content = (props: ContentProps) => {
+const ShowCodeButton = () => {
   const [showCode, , toggleCode] = SearchStorage.bool('code', 'show-code', false);
 
   return (
+    <div class="absolute right-2 bottom-1.5">
+      <ButtonIcon cross={showCode()} variant="text" icon="VsCode" onClick={toggleCode} />
+    </div>
+  );
+};
+
+export const Content = (props: ContentProps) => {
+  return (
     <div class={cx(props.class, 'w-full h-full relative')}>
-      <div class="absolute right-2 bottom-1">
-        <ButtonIcon cross={showCode()} variant="text" icon="VsCode" onClick={toggleCode} />
-      </div>
+      <ShowCodeButton />
       <Canvas />
     </div>
   );
