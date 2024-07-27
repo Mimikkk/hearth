896cbf254611953c587dfda1742afeab628dfca7
Daniel Zdancewicz <inwazzin@gmail.com>
2024-07-27 17:45:41 +0200

    tracks

--- packages/client/src/modules/renderer/engine/animation/tracks/ColorKeyframeTrack.ts
+++ packages/client/src/modules/renderer/engine/animation/tracks/ColorKeyframeTrack.ts
@@ -3,6 +3,6 @@ import { InterpolationMode } from '../../constants.js';
 
 export class ColorKeyframeTrack extends KeyframeTrack {}
 
-ColorKeyframeTrack.prototype.ValueTypeName = 'color';
+ColorKeyframeTrack.prototype.ValueTypeName = 'vector';
 ColorKeyframeTrack.prototype.ValueBufferType = Float32Array;
 ColorKeyframeTrack.prototype.DefaultInterpolation = InterpolationMode.Linear;
--- packages/client/src/modules/renderer/engine/animation/tracks/NumberKeyframeTrack.ts
+++ packages/client/src/modules/renderer/engine/animation/tracks/NumberKeyframeTrack.ts
@@ -3,6 +3,6 @@ import { InterpolationMode } from '../../constants.js';
 
 export class NumberKeyframeTrack extends KeyframeTrack {}
 
-NumberKeyframeTrack.prototype.ValueTypeName = 'number';
+NumberKeyframeTrack.prototype.ValueTypeName = 'vector';
 NumberKeyframeTrack.prototype.ValueBufferType = Float32Array;
 NumberKeyframeTrack.prototype.DefaultInterpolation = InterpolationMode.Linear;
