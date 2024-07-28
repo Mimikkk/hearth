db70776c87e4ee16f899934425243d62d1422d73
Daniel Zdancewicz <inwazzin@gmail.com>
2024-07-28 18:07:18 +0200

    ofoo

--- packages/client/src/modules/renderer/engine/renderers/BufferUtils.ts
+++ packages/client/src/modules/renderer/engine/renderers/BufferUtils.ts
@@ -1,21 +1,5 @@
 import { STD140ChunkBytes } from './constants.js';
 
 export function getFloatLength(floatLength: number): number {
-  // ensure chunk size alignment (STD140 layout)
-
   return floatLength + ((STD140ChunkBytes - (floatLength % STD140ChunkBytes)) % STD140ChunkBytes);
-}
-
-export function getVectorLength(count: number, vectorLength: number = 4): number {
-  const strideLength = getStrideLength(vectorLength);
-
-  const floatLength = strideLength * count;
-
-  return getFloatLength(floatLength);
-}
-
-export function getStrideLength(vectorLength: number): number {
-  const strideLength = 4;
-
-  return vectorLength + ((strideLength - (vectorLength % strideLength)) % strideLength);
 }
