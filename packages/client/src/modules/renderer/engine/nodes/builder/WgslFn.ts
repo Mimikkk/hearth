15eceda6139d408927718eb40aee6bd137dbf530
Daniel Zdancewicz <inwazzin@gmail.com>
2024-08-02 23:14:40 +0200

    restore ping pong

--- packages/client/src/modules/renderer/engine/nodes/builder/WgslFn.ts
+++ packages/client/src/modules/renderer/engine/nodes/builder/WgslFn.ts
@@ -3,6 +3,61 @@ import { TypeName } from '@modules/renderer/engine/nodes/builder/NodeBuilder.typ
 const declarationRegexp = /^[fn]*\s*([a-z_0-9]+)?\s*\(([\s\S]*?)\)\s*[\->]*\s*([a-z_0-9]+)?/i;
 const propertiesRegexp = /[a-z_0-9]+|<(.*?)>+/gi;
 
+const coerceType = (type: string): TypeName => {
+  switch (type) {
+    case 'vec2f':
+      return TypeName.vec2;
+    case 'vec3f':
+      return TypeName.vec3;
+    case 'vec4f':
+      return TypeName.vec4;
+    case 'mat2x2f':
+      return TypeName.mat2;
+    case 'mat3x3f':
+      return TypeName.mat3;
+    case 'mat4x4f':
+      return TypeName.mat4;
+    case 'vec2b':
+      return TypeName.bvec2;
+    case 'vec3b':
+      return TypeName.bvec3;
+    case 'vec4b':
+      return TypeName.bvec4;
+    case 'mat2x2b':
+      return TypeName.bmat2;
+    case 'mat3x3b':
+      return TypeName.bmat3;
+    case 'mat4x4b':
+      return TypeName.bmat4;
+    case 'vec2i':
+      return TypeName.ivec2;
+    case 'vec3i':
+      return TypeName.ivec3;
+    case 'vec4i':
+      return TypeName.ivec4;
+    case 'mat2x2i':
+      return TypeName.imat2;
+    case 'mat3x3i':
+      return TypeName.imat3;
+    case 'mat4x4i':
+      return TypeName.imat4;
+    case 'vec2u':
+      return TypeName.uvec2;
+    case 'vec3u':
+      return TypeName.uvec3;
+    case 'vec4u':
+      return TypeName.uvec4;
+    case 'mat2x2u':
+      return TypeName.umat2;
+    case 'mat3x3u':
+      return TypeName.umat3;
+    case 'mat4x4u':
+      return TypeName.umat4;
+    default:
+      return type as TypeName;
+  }
+};
+
 const parseWgsl = (
   source: string,
 ): {
@@ -37,12 +92,12 @@ const parseWgsl = (
     const type = propsMatches[i++][0] as TypeName;
     if (i < propsMatches.length && propsMatches[i][0].startsWith('<')) i++;
 
-    inputs.push({ type, name });
+    inputs.push({ type: coerceType(type), name });
   }
 
   const blockCode = source.substring(declaration[0].length);
   const name = declaration[1] !== undefined ? declaration[1] : '';
-  const type = (declaration[3] || 'void') as TypeName;
+  const type = declaration[3] ? coerceType(declaration[3]) : TypeName.void;
 
   return {
     type,
