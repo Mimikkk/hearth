8db357aac88c442537b28a163063c23032042b67
Daniel Zdancewicz <inwazzin@gmail.com>
2024-08-04 02:50:56 +0200

    types

--- packages/client/src/modules/renderer/engine/nodes/noise/nodes.ts
+++ packages/client/src/modules/renderer/engine/nodes/noise/nodes.ts
@@ -16,8 +16,13 @@ import { i32 } from '../shadernode/ShaderNodes.js';
 
 export const Noise = {
   fractal: {
-    f32: (position = uv(), octaves = 3, lacunarity = 2, diminish = 0.5, amplitude = 1) =>
-      fractal_noise_f32(position, i32(octaves), lacunarity, diminish).mul(amplitude),
+    f32: (
+      position = uv(),
+      octaves: number = 3,
+      lacunarity: number = 2,
+      diminish: number = 0.5,
+      amplitude: number = 1,
+    ) => fractal_noise_f32(position, i32(octaves), lacunarity, diminish).mul(amplitude),
     vec2: (position = uv(), octaves = 3, lacunarity = 2, diminish = 0.5, amplitude = 1) =>
       fractal_noise_vec2(position, i32(octaves), lacunarity, diminish).mul(amplitude),
     vec3: (position = uv(), octaves = 3, lacunarity = 2, diminish = 0.5, amplitude = 1) =>
@@ -27,14 +32,16 @@ export const Noise = {
   },
 
   perlin: {
-    f32: (position = uv(), amplitude = 1, pivot = 0) => perlin_noise_f32(position).mul(amplitude).add(pivot),
-    vec3: (position = uv(), amplitude = 1, pivot = 0) => perlin_noise_vec3(position).mul(amplitude).add(pivot),
+    f32: (position = uv(), amplitude: number = 1, pivot: number = 0) =>
+      perlin_noise_f32(position).mul(amplitude).add(pivot),
+    vec3: (position = uv(), amplitude: number = 1, pivot: number = 0) =>
+      perlin_noise_vec3(position).mul(amplitude).add(pivot),
   },
 
   worley: {
-    f32: (position = uv(), jitter = 1) => worley_noise_f32(position, jitter, i32(1)),
-    vec2: (position = uv(), jitter = 1) => worley_noise_vec2(position, jitter, i32(1)),
-    vec3: (position = uv(), jitter = 1) => worley_noise_vec3(position, jitter, i32(1)),
+    f32: (position = uv(), jitter: number = 1) => worley_noise_f32(position, jitter, i32(1)),
+    vec2: (position = uv(), jitter: number = 1) => worley_noise_vec2(position, jitter, i32(1)),
+    vec3: (position = uv(), jitter: number = 1) => worley_noise_vec3(position, jitter, i32(1)),
   },
 
   cell: {
