c8e4c7d0297a736c471caaeb9b192388dc39dbc5
Daniel Zdancewicz <inwazzin@gmail.com>
2024-10-28 20:05:43 +0100

    style

--- workspace/apps/web/src/app/layout/footer/devtools/DevtoolsButton.tsx
+++ workspace/apps/web/src/app/layout/footer/devtools/DevtoolsButton.tsx
@@ -6,11 +6,11 @@ interface DevelopmentToolsButtonProps {
 }
 
 export const DevelopmentToolsButton = (props: DevelopmentToolsButtonProps) => {
-  const Devtools = useDevTools();
+  const { active, toggle } = useDevTools();
 
   return (
-    <div class={props.class} title={Devtools.active() ? "Close development tools" : "Open development tools"}>
-      <ButtonIcon cross={Devtools.active()} icon="CgToolbox" variant="text" onClick={Devtools.toggle} />
+    <div class={props.class} title={active() ? "Close development tools" : "Open development tools"}>
+      <ButtonIcon cross={active()} icon="CgToolbox" variant="text" onClick={toggle} />
     </div>
   );
 };
