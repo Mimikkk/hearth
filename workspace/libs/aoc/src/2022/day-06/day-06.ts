ff5492a4e4fe1c5c979b04287fb966d52b9f8a83
Daniel Zdancewicz <inwazzin@gmail.com>
2024-11-26 23:39:15 +0100

    fix res

--- workspace/libs/aoc/src/2022/day-06/day-06.ts
+++ workspace/libs/aoc/src/2022/day-06/day-06.ts
@@ -1,9 +1,8 @@
-
 import { Puzzle } from "../../types/puzzle.ts";
 import { Str } from "../../utils/strs.ts";
 
 export default Puzzle.create({
-  prepare: () => Str.lines,
+  prepare: Str.trim,
   easy: () => 0,
   hard: () => 0,
 });
