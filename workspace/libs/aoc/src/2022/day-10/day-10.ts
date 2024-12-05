7b8a713d7c599f5f7ea0a7ca53a496e6e76abea9
Daniel Zdancewicz <inwazzin@gmail.com>
2024-12-05 16:05:33 +0100

    day-10 hard

--- workspace/libs/aoc/src/2022/day-10/day-10.ts
+++ workspace/libs/aoc/src/2022/day-10/day-10.ts
@@ -42,8 +42,52 @@ const sumSignalStrength = (instructions: Instruction[]): number => {
   return sum;
 };
 
+const readCrtMessage = (instructions: Instruction[]): string => {
+  let spritePosition = 1;
+
+  const width = 40;
+  const lines: string[] = [];
+
+  let cycle = 0;
+  let line = Array(width).fill(".");
+  const incrementCycle = (n?: number) => {
+    if (n !== undefined) {
+      while (n--) incrementCycle();
+      return;
+    }
+
+    const needlePosition = cycle % width;
+
+    const isVisible = needlePosition === spritePosition - 1 ||
+      needlePosition === spritePosition ||
+      needlePosition === spritePosition + 1;
+
+    if (isVisible) line[needlePosition] = "#";
+
+    if (needlePosition === 39) {
+      lines.push(line.join(""));
+      line = Array(width).fill(".");
+    }
+
+    cycle += 1;
+  };
+
+  for (let i = 0; i < instructions.length; ++i) {
+    const instruction = instructions[i];
+
+    if (isNoop(instruction)) {
+      incrementCycle(1);
+    } else {
+      incrementCycle(2);
+      spritePosition += instruction;
+    }
+  }
+
+  return lines.join("\n");
+};
+
 export default Puzzle.new({
   prepare: parseInstructions,
   easy: sumSignalStrength,
-  // hard: () => "",
+  hard: readCrtMessage,
 });
--- workspace/libs/aoc/src/utils/strs.ts
+++ workspace/libs/aoc/src/utils/strs.ts
@@ -1,10 +1,28 @@
 export namespace Str {
-  const lineRe = /\r\n|\r|\n/;
-
+  const newlineRe = /\r\n|\r|\n/;
+  const startNewlineRe = /^\r\n|\r|\n/;
   export const trim = (value: string): string => value.trim();
   export const lines = (value: string): string[] => {
-    const lines = value.split(lineRe);
+    const lines = value.split(newlineRe);
     if (lines[lines.length - 1] === "") lines.pop();
     return lines;
+  };
+
+  export const trimlines = (strings: TemplateStringsArray, ...values: unknown[]): string => {
+    const str = String.raw({ raw: strings }, ...values).replace(startNewlineRe, "");
+    const lines = Str.lines(str);
+
+    let leftOffset = 0;
+    for (let i = 0; i < lines.length; ++i) {
+      const line = lines[i];
+
+      const spaces = line.length - line.trimStart().length;
+      if (spaces > leftOffset) leftOffset = spaces;
+    }
+
+    return lines
+      .map((line) => line.trimEnd().substring(leftOffset))
+      .join("\n")
+      .trimEnd();
   };
 }
