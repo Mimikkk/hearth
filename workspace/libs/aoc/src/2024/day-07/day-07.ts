fe2e9960d78ef65e8c97cd75ff43f6bdc037ba66
Daniel Zdancewicz <inwazzin@gmail.com>
2024-12-07 12:09:15 +0100

    infer

--- workspace/libs/aoc/src/2024/day-07/day-07.ts
+++ workspace/libs/aoc/src/2024/day-07/day-07.ts
@@ -19,19 +19,18 @@ const parseEquations = (content: string): Equation[] =>
 type OperationMap = Map<string, (a: number, b: number) => number>;
 
 const validateEquation = ({ result, operands }: Equation, operations: OperationMap): boolean => {
+  const keys = operations.keys().toArray();
+
   const operandCount = operands.length;
   const operationCount = operandCount - 1;
-  const operationKeys = Array.from(operations.keys());
-
-  const combinations = operationKeys.length ** operationCount;
-
-  for (let i = 0; i < combinations; ++i) {
+  const combinationCount = keys.length ** operationCount;
+  for (let i = 0; i < combinationCount; ++i) {
     let value = operands[0];
     let combination = i;
 
     for (let j = 0; j < operationCount; ++j) {
-      const operation = operationKeys[combination % operationKeys.length];
-      combination = ~~(combination / operationKeys.length);
+      const operation = keys[combination % keys.length];
+      combination = ~~(combination / keys.length);
 
       value = operations.get(operation)!(value, operands[j + 1]);
     }
@@ -46,7 +45,7 @@ const createStrategy = (
   operations: [operator: string, resolve: (a: number, b: number) => number][],
 ): (equation: Equation) => boolean => {
   const map: OperationMap = new Map(operations);
-  return (equation: Equation): boolean => validateEquation(equation, map);
+  return (equation) => validateEquation(equation, map);
 };
 
 const isValidMulAdd = createStrategy([
@@ -60,19 +59,8 @@ const isValidMulAddJoin = createStrategy([
   ["||", (a, b) => +`${a}${b}`],
 ]);
 
-const sumValidEquationSums = (equations: Equation[], validate: (equation: Equation) => boolean) => {
-  let sum = 0;
-
-  for (let i = 0; i < equations.length; ++i) {
-    const equation = equations[i];
-
-    if (!validate(equation)) continue;
-
-    sum += equation.result;
-  }
-
-  return sum;
-};
+const sumValidEquationSums = (equations: Equation[], validate: (equation: Equation) => boolean) =>
+  equations.filter(validate).reduce((a, b) => a + b.result, 0);
 
 export default Puzzle.new({
   prepare: parseEquations,
