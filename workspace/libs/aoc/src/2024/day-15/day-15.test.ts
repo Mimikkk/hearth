9016fc848b97fa0ac6dc88c0dd1f22878156bb4d
Daniel Zdancewicz <inwazzin@gmail.com>
2024-12-19 21:06:41 +0100

    reintroduce tests

--- workspace/libs/aoc/src/2024/day-15/day-15.test.ts
+++ workspace/libs/aoc/src/2024/day-15/day-15.test.ts
@@ -3,11 +3,10 @@ import puzzle from "./day-15.ts";
 
 createPuzzleTest({
   puzzle,
-  // easyTest: 10092,
-  // easyTestInput: "type:input-test-big",
-  // easyUser: 1456590,
-  // hardTest: 9021,
-  // hardTestInput: "type:input-test-tunnel-horizontal",
-  // hardTestInput: "type:input-test-wall-split",
+  easyTest: 10092,
+  easyTestInput: "type:input-test-big",
+  easyUser: 1456590,
+  hardTest: 9021,
+  hardTestInput: "type:input-test-big",
   hardUser: 1489116,
 });
